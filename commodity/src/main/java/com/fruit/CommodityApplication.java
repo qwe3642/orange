a32b139e9a2790c47b690847ea5dcd5c31194f1f
qwe3642 <364297143@qq.com>
2020-05-12 10:44:38 +0800

    商品服务

---
+++ commodity/src/main/java/com/fruit/CommodityApplication.java
@@ -0,0 +1,13 @@
+package com.fruit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommodityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommodityApplication.class, args);
+    }
+
+}
